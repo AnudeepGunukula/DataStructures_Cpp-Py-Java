5c1289c5d5e0f3f90f0c996480e3f4fef04e6035
Anudeep Gunukula <anudeepgunukula@gmail.com>
2022-03-09 10:41:21 +0000

    array rev

--- STL/Array.cpp
+++ STL/Array.cpp
@@ -1,27 +1,27 @@
-#include <bits/stdc++.h>
 #include <array>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    array<int,5> arr={1,2,3,4,5};
-    cout<<"The elements in array are: ";
-    for(int i=0;i<5;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-    
-    cout<<"The element at position 2 is "<<arr.at(1)<<endl;
+int main() {
+  array<int, 5> arr = {1, 2, 3, 4, 5};
+  cout << "The elements in array are: ";
+  for (int i = 0; i < 5; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
 
-    cout<<"is array empty? "<<arr.empty()<<endl;
+  cout << "The element at position 2 is " << arr.at(1) << endl;
 
-    cout<<"First Element is: "<<arr.front()<<endl;
+  cout << "is array empty? " << arr.empty() << endl; // checks size of array
 
-    cout<<"Last Element is: "<<arr.back()<<endl;
+  cout << "First Element is: " << arr.front() << endl;
 
+  cout << "First Element is: " << arr[0] << endl;
 
-    return 0;
+  cout << "Last Element is: " << arr.back() << endl;
+
+  cout << "Last Element is: " << arr[sizeof(arr) / sizeof(arr[0]) - 1] << endl;
+
+  return 0;
 }
-
